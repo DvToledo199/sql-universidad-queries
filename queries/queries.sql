1e8b7280c9d56349b70ec9f45b3b1225494edd0d
DvToledo199 <toledo_1990@hotmail.com>
2026-03-24 20:05:27 +0100

    Update queries 5

--- queries/queries.sql
+++ queries/queries.sql
@@ -20,7 +20,9 @@ FROM persona
 WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%k';
 
 -- 5. Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7. (id, nombre, cuatrimestre, curso, id_grado)
-
+SELECT id, nombre, cuatrimestre, curso, id_grado
+FROM asignatura
+WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
 
 -- 6. Retorna un llistat dels professors/es juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom. (apellido1, apellido2, nombre, departamento)
 
